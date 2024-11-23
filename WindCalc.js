1479e19a08ed08f6714242bffc1cd6ebcc7af90c
Joshua Daniel Chance <joshchance78@yahoo.com>
2024-11-23 12:20:55 -0800

    Update WindCalc.js
    changed variables and included if - else statements

--- WindCalc.js
+++ WindCalc.js
@@ -1,29 +1,35 @@
-
-
 function windCalulator(direction, runway, winds) {
-  let header = "Headwind = ";
+ 
+  const header = "Headwind = ";
   let angleInDegrees = direction - runway;
   let angleInRadians = angleInDegrees * (Math.PI / 180);
   let cosineValue = Math.cos(angleInRadians);
   let headWind = Math.abs(winds * cosineValue);
 
-  let crosser = "Crosswind = ";
+  const crosser = "Crosswind = ";
   let angleInDegreesCw = direction - runway;
   let angleInRadiansCw = angleInDegreesCw * (Math.PI / 180);
   let cosineValueCw = Math.sin(angleInRadiansCw);
   let crossWindCw = Math.abs(winds * cosineValueCw);
 
-  let tail = "Tailwind = ";
+
+  const tail = "Tailwind = ";
   let angleInDegreesTw = direction - runway;
   let angleInRadiansTw = angleInDegreesTw * (Math.PI / 180);
   let cosineValueTw = Math.cos(angleInRadiansTw);
   let crossWindTw = Math.abs(winds * cosineValueTw);
 
-  return (`${crosser}`) + Math.floor(crossWindCw) + " " + '\n' + (`${header}`) + Math.floor(headWind);
+  if (direction === runway) {
+    return (`${header}`) + Math.floor(headWind);
+
+  } else if(direction >= 30 && direction <= 59);
+    return (`${crosser}`) + Math.floor(crossWindCw) + " " + '\n' + (`${header}`) + Math.floor(headWind);
+
+ //  + " " + '\n' + (`${header}`) + Math.floor(headWind) + " " + '\n' + (`${tail}`) + Math.floor(crossWindTw);
 }
 
 
-console.log(windCalulator(80, 80, 20));
+console.log(windCalulator(310, 260, 20));
 
 
 
