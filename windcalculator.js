a02724e5c9d90d18c7355a1bacd877a327e1df43
Joshua Daniel Chance <joshchance78@yahoo.com>
2024-11-30 21:49:08 -0800

    Add files via upload

---
+++ windcalculator.js
@@ -0,0 +1,36 @@
+function windCalulator(direction, runway, winds) {
+
+const degreesOff = direction - runway;
+
+    const header = "Headwind = ";
+    let angleInDegrees = direction - runway;
+    let angleInRadians = angleInDegrees * (Math.PI / 180);
+    let cosineValue = Math.cos(angleInRadians);
+    let headWind = Math.abs(winds * cosineValue);
+
+    const crosser = "Crosswind = ";
+    let angleInDegreesCw = direction - runway;
+    let angleInRadiansCw = angleInDegreesCw * (Math.PI / 180);
+    let cosineValueCw = Math.sin(angleInRadiansCw);
+    let crossWindCw = Math.abs(winds * cosineValueCw);
+
+    const tail = "Tailwind = ";
+    let angleInDegreesTw = direction - runway;
+    let angleInRadiansTw = angleInDegreesTw * (Math.PI / 180);
+    let cosineValueTw = Math.cos(angleInRadiansTw);
+    let crossWindTw = Math.abs(winds * cosineValueTw);
+
+    if (degreesOff == 0) {
+        return (`${header}`) + Math.floor(headWind);
+
+    } else if (degreesOff >= 20 && degreesOff <= 60) {
+        return (`${crosser}`)+ Math.floor(crossWindCw) + " " + '\n' + (`${header}`) + Math.floor(headWind);
+
+    } else if (degreesOff > 60 || degreesOff != headWind) {
+        return (`${crosser}`) + Math.floor(crossWindCw) + " " + '\n' + (`${tail}`) + Math.floor(crossWindTw);
+    }
+
+  return "no winds";
+  }
+console.log(windCalulator(80, 260, 20));
+
