f38e6b80d3c20acb27625ef5da935f22ec24aaf0
Joshua Daniel Chance <joshchance78@yahoo.com>
2025-04-06 20:25:53 -0700

    Update Distance.js
    Removed all commented out code except for the 3d function.

--- Distance.js
+++ Distance.js
@@ -1,24 +1,3 @@
-//function for distance//
-
-// function distance(speed, time){
-// const distance = Math.trunc(speed * time)
-// return distance;
-// }
-
-// console.log(distance(100, 10));
-
-//--------------------------------------------//
-
-//Distance function for 2D//
-
-// function distancePy(x2, x1, y2, y1){
-
-//     const distance = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
-//     return distance.toFixed(2);
-// 
-
-//--------------------------------------------//
-
 //Distance function for 3D map
 
 function distancePy(x2, x1, y2, y1, z2, z1){
